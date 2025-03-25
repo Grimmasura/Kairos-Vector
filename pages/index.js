5ce7bc347eff077d2ef3d8ff8a02b8f2fc6e9ade
Joshua Humphrey <joshuahumphrey@duck.com>
2025-03-25 14:48:16 -0600

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 
 import Head from 'next/head'
 import { useState } from 'react'
-import styles from '../styles/global.css'
 
 export default function Home() {
   const [revealed, setRevealed] = useState(false)
